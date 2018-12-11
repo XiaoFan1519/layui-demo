56fdf01a50b12d1d2249ee97f41c7130e6179d2b
Fan <xiaofanms@outlook.com>
2018-12-11 16:28:38 +0800

    菜单收缩后将文字隐藏。

--- js/index.js
+++ js/index.js
@@ -5,8 +5,9 @@
 layui.define(['jquery', 'element', 'layer'], function (exports) {
   var layer = layui.layer;
   var $ = layui.$;
+  var element = layui.element;
 
-  $('#LAY_app_flexible').click(function () {
+  function toggleMenu() {
     const className = 'layui-layout-custom-shrink';
     var layApp = $('#LAY_app');
     if (layApp.hasClass(className)) {
@@ -14,8 +15,16 @@ layui.define(['jquery', 'element', 'layer'], function (exports) {
     } else {
       layApp.addClass(className);
     }
+  }
 
+  $('#LAY_app_flexible').click(function () {
+    toggleMenu();
     layer.msg('Hello World');
+  });
+
+  $('.layui-side .layui-nav-item > a').live('click', function () {
+    toggleMenu();
+    return true;
   });
 
   exports('index', {}); //注意，这里是模块输出的核心，模块名必须和use时的模块名一致
