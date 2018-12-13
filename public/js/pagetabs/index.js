2186e5d7ade4a66f48eafb3d5aac8ee7c78dcc6a
Fan <xiaofanms@outlook.com>
2018-12-13 10:28:28 +0800

    完成点击选项卡，主体内容的展示以及选项卡被删除后移除对应的主体信息。

--- public/js/pagetabs/index.js
+++ public/js/pagetabs/index.js
@@ -1,27 +1,55 @@
 /**
   扩展一个test模块
-**/      
- 
-layui.define(['jquery', 'element'], function(exports){ //提示：模块也可以依赖其它模块，如：layui.define('layer', callback);
+**/
+
+layui.define(['jquery', 'element'], function (exports) { //提示：模块也可以依赖其它模块，如：layui.define('layer', callback);
   var element = layui.element;
-  element.on('tab(layadmin-layout-tabs)', function(data){
-    console.log(this); //当前Tab标题所在的原始DOM元素
-    alert('切换');
-  });
+  var $ = layui.jquery;
 
-  element.on('tabDelete(layadmin-layout-tabs)', function(data){
-    console.log(this); //当前Tab标题所在的原始DOM元素
-    alert('删除');
-    return false;
-  });
-  
+  const showClassName = 'layui-this';
+  var tabContainer = $('#LAY_app_tabsheader');
+  var iframeContainer = $('#LAY_app_tabsbody');
 
-  var obj = {
-    hello: function(str){
-      alert('Hello '+ (str||'mymod'));
+  /*
+    return null or element
+   */
+  function findTab(href) {
+    var selector = `li[lay-attr="${href}"]`;
+    return tabContainer.find(selector);
+  }
+
+  function addTab(title, href) {
+    var element = findTab(href);
+    if (element != null) {
+      showTab(element);
+      return;
     }
-  };
-  
+
+    var tab = `<li lay-attr="${href}"><span>${title}</span></li>`;
+    var iframe = `<iframe src="${href}" frameborder="0" class="layadmin-iframe"></iframe>`;
+  }
+
+  function showTab(element) {
+    element.addClass(showClassName).siblings().removeClass(showClassName);
+    var index = tabContainer.find('li').index(element);
+    iframeContainer.find('iframe').eq(index).addClass(showClassName).siblings().removeClass(showClassName);
+  }
+
+  function deleteTab(index) {
+    iframeContainer.find('iframe').eq(index).remove();
+  }
+
+  element.on('tab(layadmin-layout-tabs)', function (data) {
+    showTab($(this));
+  });
+
+  element.on('tabDelete(layadmin-layout-tabs)', function (data) {
+    console.log(data);
+    deleteTab(data.index);
+  });
+
   // 输出接口
-  exports('pagetabs', obj);
+  exports('pagetabs', {
+    addTab
+  });
 });
